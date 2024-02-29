a5cbd2fc1f069ad9611f991bfe23b71efb3d38a2
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-02-29 03:23:56 -0800

    Configuration management - Fix task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,24 @@
 #!/usr/bin/pup
-# install flask version (2.1.0) from pip3, using puppet
-package {'flask':
+# install Python version (3.8.10)
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# install pip
+package { 'python3-pip':
+  ensure => present,
+}
+
+# install flask version (2.1.0) from pip
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug version (2.1.1)
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
