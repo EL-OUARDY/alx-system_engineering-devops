0a5662c6a2a0e823e293715a272e1ac6ca9c9040
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-05-07 16:44:08 +0100

    Advanced API - task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,12 +19,16 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "Mozilla/5.0"}
 
     # call API and get data
-    response = requests.get(endpoint, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        if response.json():
+    try:
+        response = requests.get(
+            endpoint, headers=headers, allow_redirects=False
+        )
+        if response.status_code == 200:
             subscribers = (
                 response.json().get("data", {}).get("subscribers", None)
             )
             if subscribers:
                 return subscribers
+    except:
+        pass
     return 0
