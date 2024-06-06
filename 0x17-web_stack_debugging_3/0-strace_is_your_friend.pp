c75f48d0d2c33dc8d3fa3015640ffe4326fe409b
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-06-06 12:45:43 +0100

    Delete projects - Defer to cohort 20

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,8 +0,0 @@
-# puppet manuscript to fix mispelling "phpp" to "php"
-
-$file = '/var/www/html/wp-settings.php'
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file}",
-  path    => ['/bin','/usr/bin']
-}
